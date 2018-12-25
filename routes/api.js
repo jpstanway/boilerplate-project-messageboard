b34bf4dd1242432fafa4b743efd3c4e546f5d91d
Jordan S <jpstanway@gmail.com>
2018-12-24 19:45:54 -0800

    recreate reply route with mongodb syntax

--- routes/api.js
+++ routes/api.js
@@ -118,160 +118,124 @@ module.exports = function(app) {
   // REPLY ROUTE
   app
     .route("/api/replies/:board")
-    .get((req, res) => {
-      const board = req.params.board;
-      const threadId = req.query.thread_id;
-
-      if (!hexTest(threadId)) {
-        return res.send("incorrect id format");
-      }
-
-      MongoClient.connect(
-        MONGODB_CONNECTION_STRING,
-        (err, db) => {
-          if (err) return res.send("Failed to connect to database");
-
-          db.collection(board).find(
-            { _id: ObjectId(threadId) },
-            {
-              reported: 0,
-              delete_password: 0,
-              "replies.reported": 0,
-              "replies.delete_password": 0
-            },
-            (err, data) => {
-              if (err) return res.send("Failed to retrieve thread results");
-
-              res.json(data);
-            }
-          );
-        }
-      );
-    })
     .post((req, res) => {
-      const board = req.params.board;
+      const board = req.body.board;
       const threadId = req.body.thread_id;
       const text = req.body.text;
       const password = req.body.delete_password;
       const replyId = req.body.reply_id; // for testing only
 
-      if (!hexTest(threadId)) {
-        return res.send("incorrect id format");
-      }
-
-      // create new reply
       const reply = {
         _id: replyId || ObjectId(),
         text: text,
+        created_on: new Date(),
+        reported: false,
         delete_password: password
       };
 
       MongoClient.connect(
-        MONGODB_CONNECTION_STRING,
+        CONNECTION,
         (err, db) => {
           if (err) return res.send("Failed to connect to database");
 
-          // check for reply (for testing)
-          db.collection(board).find(
-            {
-              _id: ObjectId(threadId),
-              replies: {
-                $elemMatch: { _id: ObjectId(replyId) }
+          db.collection(board)
+            .findOneAndUpdate(
+              { _id: ObjectId(threadId) },
+              {
+                $set: { bumped_on: new Date() },
+                $push: { replies: reply }
               }
-            },
-            (err, data) => {
-              if (err) {
-                return res.send("Error searching database");
-              } else if (data.length === 0) {
-                db.collection(board).updateOne(
-                  { _id: ObjectId(threadId) },
-                  {
-                    bumped_on: new Date(),
-                    $push: { replies: reply }
-                  },
-                  (err, data) => {
-                    if (err) return res.send("Failed to update thread");
-                  }
-                );
+            )
+            .then(thread => res.redirect(`/b/${board}/${threadId}`))
+            .catch(err => res.json(err));
+        }
+      );
+    })
+    .get((req, res) => {
+      const board = req.params.board;
+      const threadId = req.query.thread_id;
+
+      MongoClient.connect(
+        CONNECTION,
+        (err, db) => {
+          if (err) return res.send("Failed to connect to database");
+
+          db.collection(board)
+            .findOne(
+              { _id: ObjectId(threadId) },
+              {
+                reported: 0,
+                delete_password: 0,
+                "replies.reported": 0,
+                "replies.delete_password": 0
               }
-              res.redirect(`/b/${board}/${threadId}`);
-            }
-          );
+            )
+            .then(thread => res.json(thread))
+            .catch(err => res.json(err));
         }
       );
     })
     .put((req, res) => {
-      const board = req.params.board;
+      const board = req.body.board;
       const threadId = req.body.thread_id;
       const replyId = req.body.reply_id;
 
-      if (!hexTest(threadId) || !hexTest(replyId)) {
-        return res.send("incorrect id format");
-      }
-
       MongoClient.connect(
-        MONGODB_CONNECTION_STRING,
+        CONNECTION,
         (err, db) => {
           if (err) return res.send("Failed to connect to database");
 
-          db.collection(board).findOneAndUpdate(
-            {
-              _id: ObjectId(threadId),
-              replies: {
-                $elemMatch: { _id: ObjectId(replyId) }
+          db.collection(board)
+            .findOneAndUpdate(
+              {
+                _id: ObjectId(threadId),
+                replies: { $elemMatch: { _id: ObjectId(replyId) } }
+              },
+              {
+                $set: { "replies.$.reported": true }
               }
-            },
-            { $set: { "replies.$.reported": true } },
-            (err, result) => {
-              if (err) {
-                return res.send("Error finding reply");
-              } else if (result) {
-                res.json("success");
-              } else {
-                res.json("reply doesn't exist");
-              }
-            }
-          );
+            )
+            .then(thread => res.json("success"))
+            .catch(err => res.json(err));
         }
       );
     })
     .delete((req, res) => {
-      const board = req.params.board;
+      const board = req.body.board;
       const threadId = req.body.thread_id;
       const replyId = req.body.reply_id;
       const password = req.body.delete_password;
 
-      if (!hexTest(threadId) || !hexTest(replyId)) {
-        return res.send("incorrect id format");
-      }
-
       MongoClient.connect(
-        MONGODB_CONNECTION_STRING,
+        CONNECTION,
         (err, db) => {
           if (err) return res.send("Failed to connect to database");
 
-          // first find the thread
-          db.collection(board).findOneAndUpdate(
-            {
+          db.collection(board)
+            .findOne({
               _id: ObjectId(threadId),
-              replies: {
-                $elemMatch: {
-                  _id: ObjectId(replyId),
-                  delete_password: password
-                }
-              }
-            },
-            { $set: { "replies.$.text": "[deleted]" } },
-            (err, reply) => {
-              if (err) {
-                return res.send("Error finding thread");
-              } else if (reply) {
-                res.json("success");
+              replies: { $elemMatch: { _id: ObjectId(replyId) } }
+            })
+            .then(thread => {
+              const replyToDelete = thread.replies
+                .map(reply => reply._id.toString())
+                .indexOf(replyId);
+
+              if (password === thread.replies[replyToDelete].delete_password) {
+                db.collection(board)
+                  .updateOne(
+                    {
+                      _id: ObjectId(threadId),
+                      replies: { $elemMatch: { _id: ObjectId(replyId) } }
+                    },
+                    { $set: { "replies.$.text": "[deleted]" } }
+                  )
+                  .then(reply => res.json("success"));
               } else {
                 res.json("incorrect password");
               }
-            }
-          );
+            })
+            .catch(err => res.json(err));
         }
       );
     });
